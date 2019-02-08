ebfb4f63e47d6a5eeb48c2b5bcc09f0cba8dfa60
Alexander Buzachero <alexbuzachero@gmail.com>
2019-02-08 00:01:55 -0800

    Login/Register Page

--- app/src/main/java/com/example/hackachieve/MainActivity.java
+++ app/src/main/java/com/example/hackachieve/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.hackachieve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        regButton = (Button) findViewById(R.id.registerButton);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterPage();
+            }
+        });
+    }
+
+    public void openRegisterPage(){
+        Intent intent = new Intent(this, RegisterPage.class);
+        startActivity(intent);
     }
 }
